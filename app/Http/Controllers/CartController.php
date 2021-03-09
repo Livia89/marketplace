655cfdcafc7a5a5c0cf48bb2d1fc87c72d540439
Livia89 <liviapriscilla1989@gmail.com>
2021-03-09 15:41:05 +0000

    delete  object of cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -32,4 +32,22 @@ class CartController extends Controller
         flash('Produto adicionado no carrinho')->success();
         return redirect()->route('product.single', ['slug' => $product["slug"]]);
     }
+
+    public function remove($slug)
+    {
+        
+        if(!session()->has("cart")) return redirect()->route('cart.index');
+
+        $products = session()->get('cart');
+
+        // Passa por cada registo e verifica se o slug é diferente do enviado
+        $products = array_filter($products, function($line) use($slug){
+            return $line["slug"] != $slug;
+        });
+
+        session()->put('cart', $products);
+        // flash("Artigo removido do carrinho")->success();
+        return redirect()->route('cart.index');
+
+    }
 }
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -7,41 +7,45 @@
             <h2>Carrinho de compras</h2>
         </div>
         <div class="col-12">
-            <table class="table table-stripped">
-                <thead>
+           @if ($cart)
+           <table class="table table-stripped">
+            <thead>
+                <tr>
+                    <th>Produto</th>
+                    <th>Preço</th>
+                    <th>Quantidade</th>
+                    <th>Subtotal</th>
+                    <th>Ação</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php
+                    $subtotal = 0;
+                @endphp
+                @foreach ($cart as $c)
                     <tr>
-                        <th>Produto</th>
-                        <th>Preço</th>
-                        <th>Quantidade</th>
-                        <th>Subtotal</th>
-                        <th>Ação</th>
+                        <td>{{$c['name']}}</td>
+                        <td>{{number_format($c['price'], '2', ',', '.')}}€</td>
+                        <td>{{$c['amount']}}</td>
+                        @php
+                            $subtotal += $c['price'] * $c['amount'];
+                        @endphp
+                        <td>{{number_format(($c['price'] * $c['amount']), '2', ',', '.')}}€</td>
+                        <td>
+                            <a href="{{route('cart.remove', ["slug" => $c["slug"]])}}" class="btn btn-sm btn-danger">Remover</a>
+                        </td>
                     </tr>
-                </thead>
-                <tbody>
-                    @php
-                        $subtotal = 0;
-                    @endphp
-                    @foreach ($cart as $c)
-                        <tr>
-                            <td>{{$c['name']}}</td>
-                            <td>{{number_format($c['price'], '2', ',', '.')}}€</td>
-                            <td>{{$c['amount']}}</td>
-                            @php
-                                $subtotal += $c['price'] * $c['amount'];
-                            @endphp
-                            <td>{{number_format(($c['price'] * $c['amount']), '2', ',', '.')}}€</td>
-                            <td>
-                                <a href="#" class="btn btn-sm btn-danger">Remover</a>
-                            </td>
-                        </tr>
-                    @endforeach
+                @endforeach
 
-                    <tr>
-                        <td colspan="3">Total:</td>
-                        <td colspan="2">{{number_format($subtotal, '2', ',', '.')}}</td>
-                    </tr>
-                </tbody>
-            </table>
+                <tr>
+                    <td colspan="3">Total:</td>
+                    <td colspan="2">{{number_format($subtotal, '2', ',', '.')}}</td>
+                </tr>
+            </tbody>
+        </table>
+        @else 
+            <div class="alert alert-warning">Carrinho vazio</div>
+        @endif
         </div>
     </div>
 
