1a5629ada7169734ef828d232b1c727b57ae6d35
Livia89 <liviapriscilla1989@gmail.com>
2021-03-19 15:14:24 +0000

    start sdk pagSeguro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        /* Inicialização do PagSeguro */ 
+        \PagSeguro\Library::initialize();
+        \PagSeguro\Library::cmsVersion()->setName("Marketplace")->setRelease("1.0.0");
+        \PagSeguro\Library::moduleVersion()->setName("Marketplace")->setRelease("1.0.0");
+
     }
 }
